9066cce73721474356be1529650ce9ec5ce175a4
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-05 04:30:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,3 +10,5 @@ end
 
 #./app.rb is our main application file, defining the controller that will power this web application. We create an Application class and inherit from Sinatra::Base to give it all of the web superpowers needed to transform the standard Ruby class into a Sinatra controller.
 
+#We tell our application to respond to HTTP GET requests to / by rendering the designated ERB template or HTML.
+
